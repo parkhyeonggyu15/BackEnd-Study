3d96c7499d90caa40b7f92f22e99a55a5df0bc8a
parkhyeonggyu15 <gudrb15@naver.com>
2026-04-08 16:51:43 +0900

    SQL

--- DataBase/[DML]SELECT.sql
+++ DataBase/[DML]SELECT.sql
@@ -1,128 +1,39 @@
 use shopdb;
-show tables;
-desc usertbl;
-desc buytbl;
-select * from usertbl;
-select * from buytbl;
 
--- 01 SELECT
-select userID,birthYear from usertbl;
-select userID as '아이디', birthYear as '생년월일' from usertbl;
-select userID as '아이디', birthYear as '생년월일', concat(mobile1, '-', mobile2) as '연락처' from usertbl;
-
--- 02 SELECT - WHERE(조건절 - 비교연산자)
-select * from usertbl where name='김경호'; -- 동등비교연산자(=)
-select * from usertbl where userid='lsg'; -- 동등비교연산자(=)
-select * from usertbl where birthyear >= 1970; -- 대소비교연산자
-select * from usertbl where height <= 170; -- 대소비교연산자
-
--- 03 SELECT - WHERE(조건절 - 논리연산자)
--- and 연산자
-select * from usertbl where birthyear>= 1970 and height>= 180;
--- or 연산자 
-select * from usertbl where birthyear>= 1970 or height>= 180;
--- between and
-select * from usertbl where height>=170 and height<= 180;
-select * from usertbl where height between 170 and 180;
-
--- in(포함문자열) ,like(포함문자열)
-select * from usertbl where addr in ('서울', '경기');
-select * from usertbl where addr= '서울' or '경기';
-
-select * from usertbl where name like '김%';
-select * from usertbl where name like '%수';
-select * from usertbl where name like '%경%';
-select * from usertbl where name like '김__';
-
-select * from buytbl;
--- 1 구매량(amount)가 5개 이상인 행을 출력
-select * from buytbl where amount>=5;
--- 2 가격이(price)가 50 이상 500 이하인 행의 userid와 prodname 만 출력
-select userid,prodname from buytbl where price >= 50 and price <= 500;
-select userid,prodname from buytbl where price between 50 and 500;
--- 3 구매량(amount)이 10 이상이거나 가격이 100 이상인 행 출력
-select * from buytbl where amount>=10 or amount>=100;
--- 4 userid가 k로 시작하는 행 출력
-select * from buytbl where userID like 'k%';
--- 5 '서적'이거나 '전자'인 행 출력
-select groupname from buytbl where groupname= '서적' or groupname='전자';
-select groupname from buytbl where groupname in ('서적', '전자');
--- 6 상품(prodName)이 책이거나 userID가 W로 끝나는 행 출력
-select * from buytbl where prodName ='책' or userid like '%w';
--- 7 groupname이 비어있지 않은 행만 출력 (!= , <>)
-select * from buytbl where groupName !='';
-select * from buytbl where groupName <>'';
-select * from buytbl where groupName is not null;
-
--- 04 SELECT 조건절 - 서브쿼리
-select * from usertbl;
--- 김경호의 키보다 큰 행을 조회
-select * from usertbl where height>(select height from usertbl where name= '김경호');
-
--- 성시경보다 나이가 많은(birthyear) 모든 행 조회
-select * from usertbl where birthyear<(select birthyear from usertbl where name='성시경');
-
--- 지역이 '경남'인 height보다 큰 모든 행 조회
--- all(모든 조건을 만족하는... and)
-select * from usertbl where height >=all(select height from usertbl where addr in ('경남') );
--- any(어느조건이든 하나이상 만족... or)
-select * from usertbl where height >=any(select height from usertbl where addr in ('경남') );
-
--- 문제 buytbl
-select * from buytbl;
--- 1 amount가 10인 행의 price보다 큰 행을 출력하세요(서브쿼리)
-select * from buytbl where price>=(select price from buytbl where amount =10);
--- 2 userID가 k로 시작하는 행의 amount보다 큰 행을 출력하세요(서브쿼리 + any)
-select * from buytbl where amount>=any(select amount from buytbl where userid like 'k%');
--- 3 amount가 5인 행의 price보다 큰 행을 출력하세요(서브쿼리 + all)
-select * from buytbl where price>=all(select price from buytbl where amount =5);
-
--- 05 Select order by
-select * from usertbl order by mDate asc;
-select * from usertbl order by mDate desc;
-select * from usertbl where birthyear >= 1970 order by mDate desc;
-select * from usertbl order by height desc ,mdate asc;
-
--- 06 distinct
-select distinct addr from usertbl;
-
--- 07 limit
-select * from usertbl;
-select * from usertbl limit 3;
-select * from usertbl limit 2,3; 
-
--- 08 테이블 복사
--- 08-01 구조 + 값 복사(PK,FK 복사 x)
-create table tbl_buy_copy(select * from buytbl);
-select * from tbl_buy_copy;
-desc tbl_buy_copy;
-desc buytbl;
-
-create table tbl_buy_copy2(select userid,prodname,amount from buytbl);
-select * from tbl_buy_copy2;
-
--- 08-02 구조만 복사(값x, PK o, FK x, Index o)
-create table tbl_buy_copy3 like buytbl;
-select * from tbl_buy_copy3;
-desc tbl_buy_copy3;
-
--- 08-03 데이터만 복사
-insert into tbl_buy_copy3 select * from buytbl where amount>=3;
+-- 01 select group by
+-- UserId별로 Amount 총합(집계함수 : sum)
+select userid,sum(amount) as '구매총량' from buytbl group by userid;
+-- userid별 amount*price의 총합(sum)
+select userid,sum(amount*price) as '구매총액' from buytbl group by userid;
+-- avg를 이용
+select userid, truncate(avg(amount*price), 2 ) as '구매평균액' from buytbl group by userid;
+-- min(), max()
+select max(height) from usertbl;
+select min(height) from usertbl;
 
 select * from usertbl;
+-- 가장 큰 키를 가지는 user 정보 확인
+select * from usertbl where height=(select max(height) from usertbl);
+-- 가장 작은 키를 가지는 user 정보확인
+select * from usertbl where height=(select min(height) from usertbl);
+-- 가장큰키와 가장 작은키의 유저정보를 한번에 출력
+select * from usertbl where height=(select min(height) from usertbl) or (select max(height) from usertbl);
+
+-- 문제
 select * from buytbl;
--- 1 userId 순으로 오름차순 정렬
-select * from buytbl order by userid asc;
--- 2 price 순으로 내림차순 정렬
-select * from buytbl order by price desc;
--- 3 amount 순으로 오름차순 prodName으로 내림차순정렬
-select * from buytbl order by amount asc, prodName desc;
--- 4 prodName을 오름차순으로 정렬시 중복 제거
-select distinct * from buytbl order by prodName asc;
--- 5 userID열의 검색시 중복된 아이디제거하고 select
-select distinct userid from buytbl;
--- 6 구매양(amount)가 3이상인 행을 prodName 내림차순으로 정렬
-select * from buytbl where amount>=3 order by prodName desc;
--- 7 usertbl의 addr 가 서울,경기인 값들을 CUsertbl에 복사
-create table Cusertbl(select addr from usertbl where addr in ('서울','경기'));
+select * from usertbl;  
+-- 1 buytbl에서 userid 별로 구매량(amount)의 합을 출력하세요
+select  userid,sum(amount) as '구매량' from buytbl group by userid;
+-- 2 usertbl에서 키의 평균값을 구하세요
+select truncate(avg(height), 1) as '평균값' from usertbl;
+-- 3 buy테이블에서 최대구매량과 최소구매량을 userid와함께 출력하세요
+select userid, max(amount) as '최대구매량', min(amount) as '최소구매량' from buytbl group by userid;
+-- 4 buytbl의 groupname 의 개수를 출력하세요
+select count(groupname) from buytbl;
+-- 5 city 테이블에서  CountryCode 별로 Population의 총합을 구하세요(world DB에서진행)
+use world;
+select CountryCode, sum(population) as '총합' from city group by CountryCode; 
+-- 6 country 테이블에서 Continent 별로 LifeExpectancy의 평균을 구하세요(world DB에서진행)
+select Continent, avg(LifeExpectancy) as '평균'from country group by continent;
+
 
