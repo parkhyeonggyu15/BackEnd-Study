4da8dbaeb03b0ae767afc590b35cb8490f1c102b
parkhyeonggyu15 <gudrb15@naver.com>
2026-04-10 15:23:20 +0900

    SQL

---
+++ DataBase/CH14_TRANSACTION.sql
@@ -0,0 +1,65 @@
+create table  if not exists  tbl_test(
+no	int primary key,
+name varchar(20),
+age int,
+gender char(1)
+);
+
+delete from tbl_test;
+insert into tbl_test values(1,'aa',66,'W'); -- commit;
+insert into tbl_test values(2,'ab',66,'W'); -- commit;
+insert into tbl_test values(3,'ac',66,'W'); -- commit;
+select * from tbl_test;
+commit;
+
+start transaction;
+	insert into tbl_test values(4,'aa',66,'W');
+	insert into tbl_test values(5,'ab',66,'W');
+	insert into tbl_test values(6,'ac',66,'W');
+rollback;
+
+start transaction;
+	savepoint s1;
+	insert into tbl_test values(4,'aa',66,'W');
+	insert into tbl_test values(5,'ab',66,'W');
+	insert into tbl_test values(6,'ac',66,'W');
+    savepoint s2;
+	insert into tbl_test values(7,'ac',66,'W');
+	insert into tbl_test values(8,'ac',66,'W');
+    savepoint s3;
+    insert into tbl_test values(9,'ac',66,'W');
+    insert into tbl_test values(10,'ac',66,'W');
+    rollback to s3;
+    rollback to s2;
+
+select * from tbl_test;
+
+
+
+-- AUTOCOMMIT 모드 비활성화
+
+
+drop procedure Tx_Test;
+DELIMITER $$
+create procedure TX_Test()
+begin
+	declare continue handler for SQLEXCEPTION
+    begin
+		show errors;
+        rollback;
+    end;
+    start transaction;
+		savepoint sp;
+		insert into tbl_test values(7,'aa',66,'W');
+		insert into tbl_test values(7,'ab',66,'W');
+		insert into tbl_test values(8,'ac',66,'W');    
+	commit;
+    release savepoint sp;
+end $$
+DELIMITER ;
+
+call TX_Test();
+show procedure status;
+select * from tbl_test;
+ 
+ 
---
+++ DataBase/CH16_TRIGGER.sql
@@ -0,0 +1,59 @@
+use shopdb;
+-- 01 After Trigger
+
+drop table c_usertbl;
+create table c_usertbl select * from usertbl;
+select * from c_usertbl;
+create table c_usertbl_bak like c_usertbl;
+select * from c_usertbl_bak;
+alter table c_usertbl_bak add column type char(5);
+alter table c_usertbl_bak add column U_D_date char(5);
+alter table c_usertbl_bak change column U_D_date U_D_date datetime;
+desc c_usertbl_bak;
+select * from c_usertbl_bak;
+
+
+delimiter $$
+create trigger trg_c_usertbl_update
+after update
+on c_usertbl
+for each row
+begin
+	insert into c_usertbl_bak values(
+    old.userid,old.name,old.birthyear,old.addr,old.mobile1,old.mobile2,old.height,
+    old.mDate,'수정',now()
+    );
+end $$
+delimiter ;
+
+show triggers;
+show create trigger trg_c_usertbl_update;
+
+select * from c_usertbl;
+select * from c_usertbl_bak;
+update c_usertbl set name='바비' where userid='BBK';
+update c_usertbl set addr='경남' where userid='EJW';
+
+
+-- 02 삭제 트리거
+delimiter $$
+create trigger trg_c_usertbl_delete
+after delete
+on c_usertbl
+for each row
+begin
+	insert into c_usertbl_bak values(
+    old.userid,old.name,old.birthyear,old.addr,old.mobile1,old.mobile2,old.height,
+    old.mDate,'삭제',now()
+    );
+end $$
+delimiter ;
+
+delete from c_usertbl where userid='KKH';
+select * from c_usertbl_bak;
+
+-- buytbl의 c_buytbl의 구조+값복사
+-- c_buytbl의 구조만 복사한 c_buytbl_bak 만들기
+-- c_buytbl_bak 에 type char(5)와 mDate datetime 을 열로 추가
+-- c_buytbl의 update시 c_buytbl_bak에 내용저장되는 trg_c_buytbl_update 트리거 만들기
+-- c_buytbl의 delete시 c_buytbl_bak에 내용저장되는 trg_c_buytbl_delete 트리거 만들기
